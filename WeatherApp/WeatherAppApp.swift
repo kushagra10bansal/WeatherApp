a7bc182db0c52c4c304241c1e16faa2813efa8b0
Kushagra <kushagra.bansal@encora.com>
2026-01-29 07:18:04 -0800

    Setup app config for dev and production environments

--- WeatherApp/WeatherAppApp.swift
+++ WeatherApp/WeatherAppApp.swift
@@ -5,9 +5,40 @@ struct WeatherAppApp: App {
     var body: some Scene {
         WindowGroup {
             RootView()
+                .onAppear {
+                    
+                    // BASE URL
+                    if let baseURL = Bundle.main.object(
+                        forInfoDictionaryKey: "BASE_URL"
+                    ) as? String {
+                        print("BASE_URL: https://\(baseURL)")
+                    } else {
+                        print("BASE_URL not found")
+                    }
+
+                    // ANALYTICS FLAG
+                    if let analytics = Bundle.main.object(
+                        forInfoDictionaryKey: "MY_ANALYTICS_ENABLED"
+                    ) as? String {
+                        print("MY_ANALYTICS_ENABLED: \(analytics)")
+                    } else {
+                        print("MY_ANALYTICS_ENABLED not found")
+                    }
+
+                    // CONFIGURATION
+                    if let config = Bundle.main.object(
+                        forInfoDictionaryKey: "Configuration"
+                    ) as? String {
+                        print("CONFIGURATION: \(config)")
+                    } else {
+                        print("Configuration not found")
+                    }
+                }
         }
     }
 }
+
 #Preview {
     RootView()
 }
+
